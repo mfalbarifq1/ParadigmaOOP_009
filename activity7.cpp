3d7356d6ffe70ad1f4119110aab0005785228bc2
mfalbarifq1 <mfalbarif.q1@gmail.com>
2026-04-21 15:17:02 +0700

    perbaikan

---
+++ activity7.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+using namespace std;
+
+class Barang {
+public :
+       string nama;
+        int jumlah;
+        string kategori;
+        char tanggalProduksi;
+
+    void SpesifikasiBarang() {
+             cout << "NAMA : " << nama << endl;
+             cout << "jumlah: " << jumlah << endl;
+             cout << "kategori : " << kategori << endl;
+             cout << "tanggalProduksi : " << tanggalProduksi << endl;
+
+  }
+};
+        
+
+int main(){
+    Barang Elektronik;
+    Elektronik.nama = "kulkas";
+    Elektronik.jumlah = 2;
+    Elektronik.kategori = "Elektronik";
+    Elektronik.SpesifikasiBarang();
+
+   
+ Barang NonElektronik;
+    NonElektronik.nama = "sepatu";
+    NonElektronik.jumlah = 10;
+    NonElektronik.kategori = "NonElektronik";
+    
+    NonElektronik.SpesifikasiBarang();
+}
